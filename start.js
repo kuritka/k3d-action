3baa071da36fd5e4bc165736dce6dd453a4885fc
kuritka <kuritka@gmail.com>
2020-12-04 13:41:45 +0100

    single cluster

--- start.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const shell = require('child_process').spawnSync;
-
-const proc = shell('bash', [path.join(__dirname, 'run.sh'), 'deploy'], {stdio: 'inherit'});
-process.exit(proc.status);
--- stop.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const shell = require('child_process').spawnSync;
-
-const proc = shell('bash', [path.join(__dirname, 'run.sh'), 'hello'], {stdio: 'inherit'});
-process.exit(proc.status);
